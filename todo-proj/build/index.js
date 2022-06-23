65c9cbbf26c7c3ff77d0e3cbf657190b3ed65b30
mirazer0 <mirazer0@naver.com>
2022-06-23 14:46:20 +0900

    테스트

--- todo-proj/build/index.js
+++ todo-proj/build/index.js
@@ -1,19 +1,15 @@
-class TodoItem {
-    constructor(id, task, complete) {
-        this.id = id;
-        this.task = task;
-        this.complete = complete;
-    }
-
-    printDetails() {
-        console.log(`#{this.id}\t${this.task}\t${this.complete ? '\t(complete)' : ''}`);
-    }
-}
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const TodoItem_1 = __importDefault(require("./TodoItem"));
 const data = [
-    {id: 1, task: '장보기', complete: true},
-    {id: 2, task: 'TS 학습하기', complete: false},
+    { id: 1, task: '장보기', complete: true },
+    { id: 2, task: 'TS 학습하기', complete: false },
 ];
-
-let todoItem = new TodoItem(data[0].id, data[1].task, data[2].complete);
-console.log(todoItem);
+console.log('My Todo List');
+for (let i = 0; i < data.length; i++) {
+    let todoItem = new TodoItem_1.default(data[i].id, data[i].task, data[i].complete);
+    todoItem.printDetails();
+}
